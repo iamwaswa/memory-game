faf7ce12f2f3300e698341635e2ec017b1e5106c
waswaolunga <olungaw@gmail.com>
2019-04-17 10:13:44 -0700

    Complete media queries

--- src/Components/BoxList/Styles.js
+++ src/Components/BoxList/Styles.js
@@ -1,43 +1,71 @@
 import styled from 'styled-components';
 
 export const ListOfBoxes = styled.main`
+  text-align: center;
   display: grid;
   grid-template-rows: repeat(${({ numBoxes }) => {
     return numBoxes / 2;
-  }}, auto);
+  }}, 100px);
   grid-template-columns: repeat(${({ numBoxes }) => {
     return 2;
-  }}, auto);
-  grid-row-gap: 1rem;
-  grid-column-gap: 1rem;
+  }}, 100px);
+  grid-row-gap: 2rem;
+  grid-column-gap: 2rem;
   padding: 2rem;
   min-width: 320px;
   min-height: 480px;
 
-  @media (min-width: 700px) {
+  @media (min-width: 428px) {
+    grid-template-rows: repeat(${({ numBoxes }) => {
+      return Math.round(numBoxes / 3);
+    }}, 100px);
+    grid-template-columns: repeat(${({ numBoxes }) => {
+      return 3;
+    }}, 100px);
+  }
+
+  @media (min-width: 560px) {
     grid-template-rows: repeat(${({ numBoxes }) => {
       return Math.round(numBoxes / 4);
-    }}, auto);
+    }}, 100px);
     grid-template-columns: repeat(${({ numBoxes }) => {
       return 4;
-    }}, auto);
+    }}, 100px);
   }
 
-  @media (min-width: 800px) {
+  @media (min-width: 698px) {
+    grid-template-rows: repeat(${({ numBoxes }) => {
+      return Math.round(numBoxes / 5);
+    }}, 100px);
+    grid-template-columns: repeat(${({ numBoxes }) => {
+      return 5;
+    }}, 100px);
+  }
+
+  @media (min-width: 820px) {
     grid-template-rows: repeat(${({ numBoxes }) => {
       return Math.round(numBoxes / 6);
-    }}, auto);
+    }}, 100px);
     grid-template-columns: repeat(${({ numBoxes }) => {
       return 6;
-    }}, auto);
+    }}, 100px);
   }
 
-  @media (min-width: 1440px) {
+  @media (min-width: 956px) {
+    grid-template-rows: repeat(${({ numBoxes }) => {
+      return Math.round(numBoxes / 7);
+    }}, 100px);
+    grid-template-columns: repeat(${({ numBoxes }) => {
+      return 7;
+    }}, 100px);
+  }
+
+  @media (min-width: 1086px) {
     grid-template-rows: repeat(${({ numBoxes }) => {
       return Math.round(numBoxes / 8);
-    }}, auto);
+    }}, 100px);
     grid-template-columns: repeat(${({ numBoxes }) => {
       return 8;
-    }}, auto);
+    }}, 100px);
   }
 `;
